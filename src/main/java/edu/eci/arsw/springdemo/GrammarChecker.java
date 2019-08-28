ec34d9f16db14475536454e1ffee1c32d9fa8357
Tylones <etiennereitz@gmail.com>
2019-08-28 08:08:09 -0500

    Introduction made

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -3,33 +3,34 @@ package edu.eci.arsw.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
 
-	SpellChecker sc;
+    @Autowired
+    SpellChecker sc;
 
-	String x;
-        
-        
-	public SpellChecker getSpellChecker() {
-		return sc;
-	}
-
-	public void setSpellChecker(SpellChecker sc) {
-		this.sc = sc;
-	}
+    String x;
 
 
-	public String check(String text){
-		
-		StringBuffer sb=new StringBuffer();
-		sb.append("Spell checking output:"+sc.checkSpell(text));
-		sb.append("Plagiarism checking output: Not available yet");
-		
-		
-		return sb.toString();
-		
-	}
+    public SpellChecker getSpellChecker() {
+            return sc;
+    }
+
+    public void setSpellChecker(SpellChecker sc) {
+            this.sc = sc;
+    }
+
+
+    public String check(String text){
+
+            StringBuffer sb=new StringBuffer();
+            sb.append("Spell checking output:"+sc.checkSpell(text));
+            sb.append("Plagiarism checking output: Not available yet");
+
+
+            return sb.toString();
+
+    }
 	
 	
 }
